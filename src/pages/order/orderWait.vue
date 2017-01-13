3e969967359ce19cac7becc3495adbfbdb7cc7b7
Yuan Han <yuanhan1890@gmail.com>
2017-01-13 13:47:34 +0800

    vue-table初始

--- src/pages/order/orderWait.vue
+++ src/pages/order/orderWait.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <div>
+      <vue-table
+        ref="table"
+        api-url="/static/fake-data/order/page-1.json"
+        :fields="fields"
+      ></vue-table>
+    </div>
   </div>
 </template>
 <script>
@@ -7,6 +14,22 @@ import VueTable from 'components/vue-table/Vuetable.vue'
 
 export default {
   data(){
+    return {
+      fields: [
+        {
+          name: 'id',
+          title: '序号',
+          dataClass: 'center aligned'
+        },
+        {
+          name: 'name',
+          title: '名称',
+          sortField: 'name'
+        }
+      ]
+    }
+  },
+  mounted(){
 
   },
   components: {
