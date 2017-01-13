0ae57bd6f03166a066f4081a0f93ac1076614f03
Yuan Han <yuanhan1890@gmail.com>
2017-01-13 11:59:29 +0800

    tinymce zh_cn support

--- src/pages/order/orderWait.vue
+++ src/pages/order/orderWait.vue
@@ -2,12 +2,18 @@
 
   <div>
     待处理订单
+    <tinymce></tinymce>
   </div>
 
 </template>
 
 <script>
-
+    import Tinymce from 'components/tinymce/Tinymce.vue'
+    export default {
+        components: {
+            Tinymce       
+        }
+    }
 </script>
 
 <style scoped>
