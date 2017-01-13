177e10978218717e3c3ce42633fca5c7d35ceb1f
quiteee <1479425361@qq.com>
2017-01-13 13:52:32 +0800

    tb

--- src/pages/message/messageChoose.vue
+++ src/pages/message/messageChoose.vue
@@ -1,7 +1,53 @@
 <template>
 	<div>
-		<div></div>
-     	<button class="ui button left floated">全选</button>
+		<h3 class="ui header">客户列表</h3>
+		<hr>
+		<div class="ui equal width grid">
+			<div class="five wide column">
+				<div class="ui checkbox">
+					<input type="checkbox">
+					<label>聪明的小明</label>
+				</div>
+			</div>
+			<div class="five wide column">
+				<div class="ui checkbox">
+					<input type="checkbox">
+					<label>聪明的小明</label>
+				</div>
+			</div>
+			<div class="five wide column">
+				<div class="ui checkbox">
+					<input type="checkbox">
+					<label>聪明的小明</label>
+				</div>
+			</div>
+			<div class="five wide column">
+				<div class="ui checkbox">
+					<input type="checkbox">
+					<label>聪明的小明</label>
+				</div>
+			</div>
+			<div class="five wide column">
+				<div class="ui checkbox">
+					<input type="checkbox">
+					<label>聪明的小明</label>
+				</div>
+			</div>
+			<div class="five wide column">
+				<div class="ui checkbox">
+					<input type="checkbox">
+					<label>聪明的小明</label>
+				</div>
+			</div>
+			<div class="five wide column">
+				<div class="ui checkbox">
+					<input type="checkbox">
+					<label>聪明的小明</label>
+				</div>
+			</div>
+		</div>
+		<br>
+     	<button class="ui button left floated" @click="chooseAll">全选</button>
      	<button class="ui button right floated">上传</button>
 	</div>
 </template>
@@ -10,6 +56,9 @@
 export default {
 	name: 'message-choose',
 	methods:{
+		chooseAll: function () {
+			$('input[type="checkbox"]').checked();
+		}
 	},
 	data () {
 	    return {
